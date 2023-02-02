fe4a1d9ce9ccbc5bbd2285102a4d4fe0fe00b5f5
Andrei Yeutukhou <stalker10eg@mail.ru>
2023-02-02 13:25:19 +0300

    random number generator function ready

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
 import './style.css'
 
+const randomNumGenerator = (n: number, m: number): number => {
+  const range = Math.abs(n - m);
+  const numInRange = Math.round(Math.random() * range);
+  const minNum = Math.min(n, m);
+  return numInRange + minNum;
+}
+
+const result = randomNumGenerator(1, 10);
+console.log(result)
