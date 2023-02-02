24d31ec9fee91d52ca8acb52f82af37f50347b29
Andrei Yeutukhou <stalker10eg@mail.ru>
2023-02-02 16:24:40 +0300

    added kichor's decision

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import './style.css'
 
-const randomNumGenerator = (n: number, m: number): number => {
-  const range = Math.abs(n - m);
-  const numInRange = Math.round(Math.random() * range);
-  const minNum = Math.min(n, m);
-  return numInRange + minNum;
+const randomNumGenerator = (numberFrom: number = 1, numberTo: number = 10): number => {
+  const rangedNum = Math.abs(numberFrom - numberTo)
+  const numInRange = Math.round(Math.random() * rangedNum)
+  const minNum = Math.min(numberFrom, numberTo)
+  return numInRange + minNum
 }
 
-const result = randomNumGenerator(1, 10);
-console.log(result)
+const getRandomNumber = new Promise((resolve, reject) => {
+  const randomNumber = randomNumGenerator()
+
+  setTimeout(() => {
+    randomNumber < 5 ? resolve(randomNumber) : reject(randomNumber)
+  }, randomNumber * 1000)
+
+})
+
+getRandomNumber
+  .then(number => console.log(`response received after ${number} sec`))
+  .catch(error => console.error(`response received after ${error} sec`))
+
+// const getRandomNumberWithDelay: Promise<number> = new Promise((resolve, reject) => {
+//   const randomNumber = Math.round(Math.random() * 10)
+//
+//   setTimeout(() => {
+//     randomNumber < 6 ? resolve(randomNumber) : reject(randomNumber)
+//   }, randomNumber * 1000)
+// })
+//
+// getRandomNumberWithDelay
+//   .then((result) => {
+//     console.log(result)
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
